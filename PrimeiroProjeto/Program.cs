52b7c9d94b315607df7de215e53aa3fcc982e4c1
Lucas Teixeira <GibirinoGamer@hotmail.com>
2023-05-26 20:53:32 -0300

    Criação do projeto

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,31 @@
+﻿// Musiquer
+string msgBoasVindas = "Boas vindas ao Musiquer";
+
+
+void ExibirBoasVindas()
+    {
+        Console.WriteLine
+        (@"
+            ███╗░░░███╗██╗░░░██╗░██████╗██╗░██████╗░██╗░░░██╗███████╗██████╗░
+            ████╗░████║██║░░░██║██╔════╝██║██╔═══██╗██║░░░██║██╔════╝██╔══██╗
+            ██╔████╔██║██║░░░██║╚█████╗░██║██║██╗██║██║░░░██║█████╗░░██████╔╝
+            ██║╚██╔╝██║██║░░░██║░╚═══██╗██║╚██████╔╝██║░░░██║██╔══╝░░██╔══██╗
+            ██║░╚═╝░██║╚██████╔╝██████╔╝██║░╚═██╔═╝░╚██████╔╝███████╗██║░░██║
+            ╚═╝░░░░░╚═╝░╚═════╝░╚═════╝░╚═╝░░░╚═╝░░░░╚═════╝░╚══════╝╚═╝░░╚═╝
+        ");
+        Console.WriteLine(msgBoasVindas);
+    }
+
+void ExibirOpcoesMenu()
+    {
+    Console.WriteLine("Digite 1 para registrar banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite 5 para sair");
+
+    }
+
+ExibirBoasVindas();    
+
+ExibirOpcoesMenu();
