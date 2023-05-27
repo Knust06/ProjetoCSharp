8c6e12b859d60396c862b8c3bf5d77519caba08d
Lucas Teixeira <GibirinoGamer@hotmail.com>
2023-05-26 21:03:53 -0300

    Criação da função Escrever

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -2,10 +2,17 @@
 string msgBoasVindas = "Boas vindas ao Musiquer";
 
 
+
+void Escrever(string msg)
+    {
+    Console.WriteLine(msg);
+}
+
+
 void ExibirBoasVindas()
     {
-        Console.WriteLine
-        (@"
+    Escrever
+    (@"
             ███╗░░░███╗██╗░░░██╗░██████╗██╗░██████╗░██╗░░░██╗███████╗██████╗░
             ████╗░████║██║░░░██║██╔════╝██║██╔═══██╗██║░░░██║██╔════╝██╔══██╗
             ██╔████╔██║██║░░░██║╚█████╗░██║██║██╗██║██║░░░██║█████╗░░██████╔╝
@@ -13,16 +20,16 @@ void ExibirBoasVindas()
             ██║░╚═╝░██║╚██████╔╝██████╔╝██║░╚═██╔═╝░╚██████╔╝███████╗██║░░██║
             ╚═╝░░░░░╚═╝░╚═════╝░╚═════╝░╚═╝░░░╚═╝░░░░╚═════╝░╚══════╝╚═╝░░╚═╝
         ");
-        Console.WriteLine(msgBoasVindas);
+    Escrever(msgBoasVindas);
     }
 
 void ExibirOpcoesMenu()
     {
-    Console.WriteLine("Digite 1 para registrar banda");
-    Console.WriteLine("Digite 2 para mostrar todas as bandas");
-    Console.WriteLine("Digite 3 para avaliar uma banda");
-    Console.WriteLine("Digite 4 para exibir a média de uma banda");
-    Console.WriteLine("Digite 5 para sair");
+    Escrever("Digite 1 para registrar banda");
+    Escrever("Digite 2 para mostrar todas as bandas");
+    Escrever("Digite 3 para avaliar uma banda");
+    Escrever("Digite 4 para exibir a média de uma banda");
+    Escrever("Digite 5 para sair");
 
     }
 
