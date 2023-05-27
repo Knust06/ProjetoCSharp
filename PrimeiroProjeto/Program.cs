c79272e936a9d3f1026fbe231163ea75baa8cf37
Lucas Teixeira <GibirinoGamer@hotmail.com>
2023-05-26 21:21:19 -0300

    Criando função Opcoes

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,20 @@ void Escrever(string msg)
     {
     Console.WriteLine(msg);
 }
-
+void EscreverAcao(string msg)
+{
+    Console.Write(msg);
+}
+void Opcaos()
+    {
+        string opcaoEscolhida = Console.ReadLine()!;
+        int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida) ;
+        if (opcaoEscolhidaNumerica == 1)
+        {
+            Escrever("Você digitou a opção " + opcaoEscolhida);
+        }
+        // ! no readline é para ser usado quando não queremos que o mesmo retorne valor nulo
+    }
 
 void ExibirBoasVindas()
     {
@@ -30,6 +43,10 @@ void ExibirOpcoesMenu()
     Escrever("Digite 3 para avaliar uma banda");
     Escrever("Digite 4 para exibir a média de uma banda");
     Escrever("Digite 5 para sair");
+
+    EscreverAcao("\nDigite a sua opção: ");
+
+    Opcaos();
 
     }
 
